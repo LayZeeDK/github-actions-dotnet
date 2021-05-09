749a354359d865a6a161c23a0127f07e535758b4
Lars Gyrup Brink Nielsen <larsbrinknielsen@gmail.com>
2021-05-09 23:12:24 +0200

    test: cover WeatherForecastController

---
+++ Lars.WeatherApi.Tests/Controllers/WeatherForecastControllerTests.cs
@@ -0,0 +1,77 @@
+using System;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Collections.Generic;
+
+namespace Lars.WeatherApi.Tests.Controllers
+{
+    public class WeatherForecastControllerTests
+        : IClassFixture<WebApplicationFactory<Lars.WeatherApi.Startup>>, IDisposable
+    {
+        private bool disposedValue;
+        protected HttpClient Http { get; }
+
+        public WeatherForecastControllerTests(WebApplicationFactory<Lars.WeatherApi.Startup> webApplicationFactory)
+        {
+            this.Http = webApplicationFactory.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            this.Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposedValue)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                this.Http.Dispose();
+            }
+
+            this.disposedValue = true;
+        }
+
+        public class Get : WeatherForecastControllerTests
+        {
+            public Get(WebApplicationFactory<Lars.WeatherApi.Startup> webApplicationFactory)
+                : base(webApplicationFactory)
+            {
+            }
+
+            [Fact]
+            public async Task RespondsWithSuccess()
+            {
+                var response = await this.Http.GetAsync("/weatherforecast");
+
+                response.EnsureSuccessStatusCode();
+            }
+
+            [Fact]
+            public async Task RespondsWithA5DayWeatherForecast()
+            {
+                var response = await this.Http.GetAsync("/weatherforecast");
+
+                var forecast = await this.ToJsonAsync<IEnumerable<WeatherForecast>>(response);
+                Assert.Equal(5, forecast.Count());
+            }
+
+            private async Task<TValue> ToJsonAsync<TValue>(HttpResponseMessage response)
+            {
+                var content = await response.Content.ReadAsStreamAsync();
+
+                return await JsonSerializer.DeserializeAsync<TValue>(content);
+            }
+        }
+    }
+}
